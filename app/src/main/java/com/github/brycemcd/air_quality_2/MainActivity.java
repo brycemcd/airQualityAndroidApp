3e0775d5997160e1d80eebb102f1ae5407476749
Bryce McDonnell <bryce@bridgetownint.com>
2019-10-04 18:29:11 -0400

    Clean up MainActivity a bit
    * It's not really useful
    * Pruned awful code. Left bad code

--- app/src/main/java/com/github/brycemcd/air_quality_2/MainActivity.java
+++ app/src/main/java/com/github/brycemcd/air_quality_2/MainActivity.java
@@ -6,25 +6,22 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothManager;
 import android.bluetooth.le.BluetoothLeScanner;
-import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
-import android.content.IntentFilter;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.content.pm.PackageManager;
 
-import java.lang.reflect.Array;
 import java.util.ArrayList;
-import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -45,15 +42,16 @@ public class MainActivity extends AppCompatActivity {
 
     final private Handler handler = new Handler();
 
+    public AdapterView.OnItemClickListener listActionlistView = new AdapterView.OnItemClickListener() {
+        @Override
+        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+            String clickString = bluetoothDevices.get(position);
+            Log.d("LIST ITEM", "clicked! " + clickString);
+        }
+    };
 
-    private void resetDeviceList() {
-        bluetoothDevices = new ArrayList<>();
-        arrayAdapter.notifyDataSetChanged();
-    }
 
     public void searchClicked(View view) {
-//        resetDeviceList();
-
         statusTextView.setText("Searching ...");
         searchButton.setEnabled(false);
         startScanning();
@@ -64,30 +62,6 @@ public class MainActivity extends AppCompatActivity {
         searchButton.setText("Scan");
         searchButton.setEnabled(true);
     }
-
-    private final BroadcastReceiver broadcastReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            Log.i("AIR ACTION", action);
-
-            if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
-                searchButton.setEnabled(true);
-                statusTextView.setText("finished");
-            } else if (BluetoothDevice.ACTION_FOUND.equals(action)) {
-               BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
-
-               String address = device.getAddress();
-               String rssi = Integer.toString(intent.getShortExtra(BluetoothDevice.EXTRA_RSSI, Short.MIN_VALUE));
-
-               Log.i("DEVICE FOUND", "ADDRESS: " + address);
-
-               bluetoothDevices.add(address);
-
-               arrayAdapter.notifyDataSetChanged();
-            }
-        }
-    };
 
 
     @Override
@@ -102,6 +76,8 @@ public class MainActivity extends AppCompatActivity {
         listView = findViewById(R.id.listView);
         statusTextView = findViewById(R.id.statusTextView);
         searchButton = findViewById(R.id.searchButton);
+
+        listView.setOnItemClickListener(listActionlistView);
 
         // Ensures Bluetooth is available on the device and it is enabled. If not,
         // displays a dialog requesting user permission to enable Bluetooth.
@@ -131,16 +107,6 @@ public class MainActivity extends AppCompatActivity {
         arrayAdapter = new ArrayAdapter(this, android.R.layout.simple_list_item_1, bluetoothDevices);
 
         listView.setAdapter(arrayAdapter);
-
-
-
-//        IntentFilter intentFilter = new IntentFilter();
-//        intentFilter.addAction(BluetoothDevice.ACTION_FOUND);
-//        intentFilter.addAction(BluetoothAdapter.ACTION_STATE_CHANGED);
-//        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
-//        intentFilter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
-//        registerReceiver(broadcastReceiver, intentFilter);
-
     }
 
     // Device scan callback.
@@ -151,8 +117,11 @@ public class MainActivity extends AppCompatActivity {
 
             Log.i("SCAN RESULT", device.getName() + " Address: " + address + " rssi: " + rssi);
 
-            bluetoothDevices.add(address);
-            arrayAdapter.notifyDataSetChanged();
+            // remove duplicates
+            if (bluetoothDevices.indexOf(address) == -1 ) {
+                bluetoothDevices.add(address);
+                arrayAdapter.notifyDataSetChanged();
+            }
 
             // Since I'm after only one device right now, we can stop as soon as it's found
             if (address.equals(DeviceControl.MAGIC_DEVICE_ADDRESS)) {
@@ -174,11 +143,6 @@ public class MainActivity extends AppCompatActivity {
                 resetUI();
             }
         }, SCAN_PERIOD);
-
-//        ArrayList<UUID> service = new ArrayList<>();
-//        // NOTE: you are here
-//        UUID airQualityService = "";
-//        service.add(airQualityService);
 
         bluetoothAdapter.startLeScan(leScanCallback);
     }
@@ -202,25 +166,4 @@ public class MainActivity extends AppCompatActivity {
     }
 
 
-//    public void listItemClick(ListView l, View v, int position, long id) {
-    public void listItemClick(View view) {
-//        final BluetoothDevice device = arrayAdapter.getDevice(position);
-        if (magicDevice == null) {
-            Log.d("onListItemClick", "NO magicDevice. HALTING");
-            return;
-        }
-
-        final BluetoothDevice device = magicDevice;
-
-        if (device == null) return;
-
-        final Intent intent = new Intent(this, DeviceControl.class);
-        intent.putExtra(DeviceControl.EXTRAS_DEVICE_NAME, device.getName());
-        intent.putExtra(DeviceControl.EXTRAS_DEVICE_ADDRESS, device.getAddress());
-//        if (mScanning) {
-//            mBluetoothAdapter.stopLeScan(mLeScanCallback);
-//            mScanning = false;
-//        }
-        startActivity(intent);
-    }
 }
